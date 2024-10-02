5ff58ea1302afb630d1b891260fd83fbb0c6bd13
RipJaw0 <theswagman2006@gmail.com>
2024-10-02 09:55:27 -0400

    Added Louis Button

--- src/main/java/org/example/demo/HelloController.java
+++ src/main/java/org/example/demo/HelloController.java
@@ -7,8 +7,15 @@ public class HelloController {
     @FXML
     private Label welcomeText;
 
+
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
+    @FXML
+    protected void onLouisButtonClick() {
+        welcomeText.setText("My Button is really cool");
+    }
+
 }
