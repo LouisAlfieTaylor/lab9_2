bec0556a1fecb46e7a3de78c7dfa37669496a4fe
LukasPerk <lukasperkov@icloud.com>
2024-10-02 10:10:55 -0400

    90ujiew

--- src/main/java/org/example/demo/HelloController.java
+++ src/main/java/org/example/demo/HelloController.java
@@ -3,12 +3,22 @@ package org.example.demo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
+import java.awt.event.ActionEvent;
+
 public class HelloController {
     @FXML
     private Label welcomeText;
 
     @FXML
+    private void onNameButtonClick() {
+        welcomeText.setText("Lukas Perkov");
+    }
+
+
+    @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
     }
+
 }
+
